4a9422761755179b7186f62ba347fd958e17bb5b
Jon Mease <jon.mease@gmail.com>
2019-10-29 14:40:04 -0400

    Fix package name

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-setup(name='hilbert_frame',
+setup(name='spatialpandas',
       packages=find_packages(exclude=('tests',)),
       install_requires=['pandas', 'dask', 'numba', 'numpy'],
-      tests_require=['pytest'])
+      tests_require=['pytest', 'hypothesis'])
