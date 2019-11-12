896688c282cb15f57add6119322d7e4add5a79fa
Jon Mease <jon.mease@gmail.com>
2019-11-12 14:13:00 -0500

    Added implementation of spatial index as Hilbert Rtree

--- setup.py
+++ setup.py
@@ -3,4 +3,4 @@ from setuptools import setup, find_packages
 setup(name='spatialpandas',
       packages=find_packages(exclude=('tests',)),
       install_requires=['pandas', 'dask', 'numba', 'numpy', 'pyarrow>=0.15'],
-      tests_require=['pytest', 'hypothesis'])
+      tests_require=['pytest', 'hypothesis', 'hilbertcurve'])
--- spatialpandas/utils.py
+++ spatialpandas/utils.py
@@ -1,3 +1,29 @@
+import numpy as np
 from numba import jit
 
 ngjit = jit(nopython=True, nogil=True)
+
+
+@ngjit
+def _data2coord(vals, val_range, n):
+    """
+    Convert an array of values from continuous data coordinates to discrete
+    integer coordinates
+
+    Args:
+        vals: Array of continuous data coordinate to be converted to discrete
+            integer coordinates
+        val_range: Tuple of start (val_range[0]) and stop (val_range[1]) range in
+            continuous data coordinates
+        n: The integer number of discrete distance coordinates
+
+    Returns:
+        unsigned integer array of discrete coordinates
+    """
+    x_width = val_range[1] - val_range[0]
+    res = ((vals - val_range[0]) * (n / x_width)).astype(np.int64)
+
+    # clip
+    res[res < 0] = 0
+    res[res > n - 1] = n - 1
+    return res
