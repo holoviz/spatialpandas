5175cfe50c735a888b13ad17b48b8ba244cf824a
Jon Mease <jon.mease@gmail.com>
2019-11-29 15:50:25 -0500

    Travis CI (#9)
    * Add geopandas to tests_require
    * Initial .travis.yaml

--- setup.py
+++ setup.py
@@ -3,4 +3,6 @@ from setuptools import setup, find_packages
 setup(name='spatialpandas',
       packages=find_packages(exclude=('tests',)),
       install_requires=['pandas', 'dask', 'numba', 'numpy', 'pyarrow>=0.15'],
-      tests_require=['pytest', 'hypothesis', 'hilbertcurve', 'shapely', 'scipy'])
+      tests_require=[
+            'pytest', 'hypothesis', 'scipy', 'shapely', 'geopandas', 'hilbertcurve'
+      ])
