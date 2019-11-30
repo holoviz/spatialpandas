4c8b7fed9ef302ca918dae18b0f37f281ed6729e
Jon Mease <jon.mease@gmail.com>
2019-11-30 08:49:03 -0500

    Use param to track current package version

--- spatialpandas/__init__.py
+++ spatialpandas/__init__.py
@@ -2,6 +2,7 @@ from . import geometry
 from . import spatialindex
 from .geoseries import GeoSeries
 from .geodataframe import GeoDataFrame
+import param
 try:
     import dask.dataframe
     # Import to trigger registration of types with Dask
@@ -9,3 +10,7 @@ try:
 except ImportError:
     # Dask dataframe not available
     pass
+
+__version__ = str(param.version.Version(
+    fpath=__file__, archive_commit="$Format:%h$", reponame="spatialpandas")
+)
