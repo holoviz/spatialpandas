975e9e57c269deb1b778b5c3f5a12c404c27c892
Jon Mease <jon.mease@gmail.com>
2019-11-30 08:49:03 -0500

    Use param to track current package version

--- setup.py
+++ setup.py
@@ -1,8 +1,32 @@
 from setuptools import setup, find_packages
+import param
+
+extras_require = {
+      'tests': [
+            'pytest',
+            'hypothesis',
+            'scipy',
+            'shapely',
+            'geopandas',
+            'hilbertcurve'
+      ],
+}
+
+install_requires = [
+      'pandas>=0.25',
+      'dask>=2.0',
+      'numba',
+      'numpy',
+      'pyarrow>=0.15',
+      'param',
+]
 
 setup(name='spatialpandas',
+      version=param.version.get_setup_version(
+            __file__, "spatialpandas", archive_commit="$Format:%h$"
+      ),
       packages=find_packages(exclude=('tests',)),
-      install_requires=['pandas>=0.25', 'dask>=2.0', 'numba', 'numpy', 'pyarrow>=0.15'],
-      tests_require=[
-            'pytest', 'hypothesis', 'scipy', 'shapely', 'geopandas', 'hilbertcurve'
-      ])
+      python_requires='>=3.6',
+      install_requires=install_requires,
+      extras_require=extras_require,
+      tests_require=extras_require['tests'])
