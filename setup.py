b3847c42db75e6d51b803a1206c5bc6026e115f2
Jon Mease <jon.mease@gmail.com>
2019-11-30 05:19:34 -0500

    update dependency versions, Python 2 not supportable since pandas 0.25 dropped support

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(name='spatialpandas',
       packages=find_packages(exclude=('tests',)),
-      install_requires=['pandas', 'dask', 'numba', 'numpy', 'pyarrow>=0.15'],
+      install_requires=['pandas>=0.25', 'dask>=2.0', 'numba', 'numpy', 'pyarrow>=0.15'],
       tests_require=[
             'pytest', 'hypothesis', 'scipy', 'shapely', 'geopandas', 'hilbertcurve'
       ])
