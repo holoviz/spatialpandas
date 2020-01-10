Fix intersection calculation on PointsArray with offset

spatialpandas/geometry/basefixed.py
@@ -184,7 +184,7 @@ class GeometryFixedArray(GeometryArray):
         if len(self.data) == 0:
             return np.array([], dtype=self.numpy_dtype)
         else:
-            start = self.data.offset
+            start = self.data.offset * self._element_len
             stop = start + len(self.data) * self._element_len
             return np.asarray(self.data.buffers()[1]).view(self.numpy_dtype)[start:stop]
 

tests/geometry/intersection/test_point_intersection.py
@@ -2,7 +2,7 @@ import shapely.geometry as sg
 import numpy as np
 import pandas as pd
 from geopandas.array import from_shapely
-from hypothesis import given, example
+from hypothesis import given, example, strategies as st
 from spatialpandas import GeoSeries
 from spatialpandas.geometry import PointArray, Point, MultiPoint, Line, MultiLine, \
     Polygon, MultiPolygon
@@ -47,6 +47,36 @@ def test_points_intersects_point(gp_points, gp_point):
         points_series.intersects(point),
         pd.Series(expected, index=points_series.index)
     )
+
+
+@given(
+    st_point_array(min_size=6),
+    st.integers(min_value=0, max_value=5),
+    st_point_array(min_size=1, max_size=1)
+)
+@hyp_settings
+def test_points_intersects_point_offset(gp_points, offset, gp_point):
+    # Get scalar Point
+    sg_point = gp_point[0]
+    if len(gp_points) > 0:
+        # Add gp_point to gp_points so we know something will intersect
+        gp_points = pd.concat([pd.Series(gp_points), pd.Series(gp_point)]).array
+
+    # Compute expected intersection
+    expected = gp_points.intersects(sg_point)[offset:]
+
+    # Create spatialpandas PointArray
+    point = Point.from_shapely(sg_point)
+    points = PointArray.from_geopandas(gp_points)[offset:]
+
+    # Test PointArray.intersect
+    result = points.intersects(point)
+    np.testing.assert_equal(result, expected)
+
+    # Test PointArray.intersects with inds
+    inds = np.flipud(np.arange(0, len(points)))
+    result = points.intersects(point, inds)
+    np.testing.assert_equal(result, np.flipud(expected))
 
 
 @given(st_point_array(), st_multipoint_array(min_size=1, max_size=1))