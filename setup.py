78cf927f9205d9bcb4caa50c196f0012b5614a47
Philipp Rudiger <prudiger@anaconda.com>
2020-08-15 03:37:06 +0200

    Add HoloViz build infrastructure

--- setup.py
+++ setup.py
@@ -30,20 +30,26 @@ install_requires = [
       'retrying',
 ]
 
-setup(name='spatialpandas',
-      version=param.version.get_setup_version(
-            __file__, "spatialpandas", archive_commit="$Format:%h$"
-      ),
-      description='Pandas extension arrays for spatial/geometric operations',
-      long_description=open("README.md").read(),
-      long_description_content_type="text/markdown",
-      url='https://github.com/holoviz/spatialpandas',
-      maintainer='Datashader developers',
-      maintainer_email='dev@datashader.org',
-      python_requires='>=3.6',
-      install_requires=install_requires,
-      extras_require=extras_require,
-      tests_require=extras_require['tests'],
-      license='BSD-2-Clause',
-      packages=find_packages(exclude=('tests', 'tests.*')),
-      include_package_data=True)
+setup_args = dict(
+    name='spatialpandas',
+    version=param.version.get_setup_version(
+        __file__, "spatialpandas", archive_commit="$Format:%h$"
+    ),
+    description='Pandas extension arrays for spatial/geometric operations',
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url='https://github.com/holoviz/spatialpandas',
+    maintainer='Datashader developers',
+    maintainer_email='dev@datashader.org',
+    python_requires='>=3.6',
+    install_requires=install_requires,
+    extras_require=extras_require,
+    tests_require=extras_require['tests'],
+    license='BSD-2-Clause',
+    packages=find_packages(exclude=('tests', 'tests.*')),
+    include_package_data=True
+)
+
+if __name__ == '__main__':
+    setup(**setup_args)
+
