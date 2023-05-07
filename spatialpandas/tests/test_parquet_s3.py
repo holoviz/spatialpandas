b9da6ca25739fb78fd7f8f484acae1022f5cb4d9
Brian Larsen <B_R_L@hotmail.com>
2023-05-07 18:55:03 -0500

    Actually skip py37

--- spatialpandas/tests/test_parquet_s3.py
+++ spatialpandas/tests/test_parquet_s3.py
@@ -19,8 +19,8 @@ geopandas = pytest.importorskip("geopandas")
 s3fs = pytest.importorskip("s3fs")
 requests = pytest.importorskip("requests")
 
-if sys.version_info < (3, 7):
-    pytest.skip("requires python3.7 or higher", allow_module_level=True)
+if sys.version_info < (3, 8):
+    pytest.skip("requires python3.8 or higher", allow_module_level=True)
 
 logging.getLogger("botocore").setLevel(logging.INFO)
 
