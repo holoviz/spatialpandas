4c480dee06da8d8651e848b37b3336c069915fa1
Simon Høxbro Hansen <simon.hansen@me.com>
2024-08-04 15:54:25 +0200

    Update conda build

---
+++ scripts/conda/build.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+set -euxo pipefail
+
+PACKAGE="spatialpandas"
+
+python -m build -w .
+
+VERSION=$(python -c "import $PACKAGE; print($PACKAGE._version.__version__)")
+export VERSION
+
+# conda config --env --set conda_build.pkg_format 2
+conda build scripts/conda/recipe --no-anaconda-upload --no-verify
+
+# mv "$CONDA_PREFIX/conda-bld/noarch/$PACKAGE-$VERSION-py_0.conda" dist
+mv "$CONDA_PREFIX/conda-bld/noarch/$PACKAGE-$VERSION-py_0.tar.bz2" dist
