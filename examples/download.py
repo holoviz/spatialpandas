7f3585cec3765b78e4534da925e79a01e5ed55fc
Simon Høxbro Hansen <simon.hansen@me.com>
2024-08-14 11:06:39 +0200

    raise exceptions if something goes wrong

--- examples/download.py
+++ examples/download.py
@@ -2,24 +2,23 @@ try:
     import requests
     from platformdirs import user_cache_path
 except ImportError:
-    raise ImportError("requests and platformdirs are needed to download data")
+    raise ImportError("requests and platformdirs are needed to download data") from None
 
 
-def download_map(dataset):
+def download_map(dataset, force=False):
     if dataset not in ("naturalearth_lowres", "naturalearth_cities"):
         raise ValueError(f"Unknown dataset: {dataset}, supported datasets are 'naturalearth_lowres' and 'naturalearth_cities'")
     url = f"https://api.github.com/repos/geopandas/geopandas/contents/geopandas/datasets/{dataset}?ref=v0.14.4"
     local_dir = user_cache_path() / "spatialpandas" / dataset
 
-    if local_dir.exists():
+    if local_dir.exists() and not force:
         return local_dir
 
     response = requests.get(url)
-    if response.status_code == 200:
+    if response.ok:
         files = response.json()
     else:
-        print(f"Failed to retrieve contents: {response.status_code}")
-        return None
+        raise ValueError(f"Failed to retrieve contents: {response.status_code}")
 
     if not local_dir.exists():
         local_dir.mkdir(parents=True)
@@ -28,7 +27,13 @@ def download_map(dataset):
         file_url = file["download_url"]
         file_name = file["name"]
         file_response = requests.get(file_url)
+        if not file_response.ok:
+            raise ValueError(f"Failed to download file: {file_name}")
         with open(local_dir / file_name, "wb") as f:
             f.write(file_response.content)
 
     return local_dir
+
+if __name__ == "__main__":
+    download_map("naturalearth_lowres")
+    download_map("naturalearth_cities")
