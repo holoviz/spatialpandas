3a1b6ebc54c107b102e055aef0f0defb68a06b1c
Simon Høxbro Hansen <simon.hansen@me.com>
2024-11-12 17:19:42 +0100

    bug: use find_spec for spatialpandas.dask import (#163)

--- spatialpandas/__init__.py
+++ spatialpandas/__init__.py
@@ -1,18 +1,16 @@
+from importlib.util import find_spec
+
 from . import geometry, spatialindex, tools
 from .__version import __version__
 from .geodataframe import GeoDataFrame
 from .geoseries import GeoSeries
 from .tools.sjoin import sjoin
 
-try:
-    import dask.dataframe  # noqa
-
+if find_spec("dask"):
     # Import to trigger registration of types with Dask
     import spatialpandas.dask  # noqa
-except ImportError:
-    # Dask dataframe not available
-    pass
 
+del find_spec
 
 __all__ = [
     "GeoDataFrame",
